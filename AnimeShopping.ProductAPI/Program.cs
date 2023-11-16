8dba8c99579017387f9c565f4f68173c87d2e075
Clenildon Ferreira <clenildonferreira34@gmail.com>
2023-11-16 16:20:59 -0300

    create migrations

--- AnimeShopping.ProductAPI/Program.cs
+++ AnimeShopping.ProductAPI/Program.cs
@@ -1,6 +1,16 @@
+using AnimeShopping.ProductAPI.Model.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var connection = builder.Configuration["MySqlConnection:MysqlConnectionString"];
+
+builder.Services.AddDbContext<MySQLContext>(options => options.
+    UseMySql(connection,
+        new MySqlServerVersion(
+            new Version(8, 0, 34))));
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
